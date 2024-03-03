c50e9a6390987e0ce1c5552959d6873cfdc370d8
chikkibum <jackiechrs10@gmail.com>
2024-03-03 16:03:44 +0530

    implemented map method for colors

--- bgchanger_react/src/App.jsx
+++ bgchanger_react/src/App.jsx
@@ -15,10 +15,11 @@ const Button = (props) => {
         {props.color}
       </button>
     </>
-  );
-};
+  )
+}
 
 function App(props) {
+  const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'pink', 'magenta'];
   const [color, setColor] = useState('powderblue');
 
   return (
@@ -26,18 +27,14 @@ function App(props) {
       <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
         <div className='fixed fle flex-wrap justify-center bottom-12 inset-x-0  px-2 '>
           <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-            <Button color='red' setColor={setColor} />
-            <Button color='green' setColor={setColor} />
-            <Button color='blue' setColor={setColor} />
-            <Button color='yellow' setColor={setColor} />
-            <Button color='purple' setColor={setColor} />
-            <Button color='pink' setColor={setColor} />
-            <Button color='magenta' setColor={setColor} />
+            {colors.map((color) => (
+              <Button key={color} color={color} setColor={setColor} />
+            ))}
           </div>
         </div>
       </div>
     </>
-  );
+  )
 }
 
 export default App
